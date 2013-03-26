09e76deb0a71887d8b4505c1afdbf41ad327119c
Drvanon <robbiedorstijn@gmail.com>
2013-03-26 22:04:18 +0100

    Do a little trick with arguments on path

--- algo.py
+++ algo.py
@@ -1,24 +1,30 @@
-queue = []
-complete = []
+import path
 
-while True:
-    path = queue.pop()
-    node = path[-1]
-    for nb in node.neighbours:
-        # don't go backwards
-        if nb in path: continue
-        new_path = path.add_node(nb)
+def find(begin, goal):
+    queue = []
+    complete = []
 
-        # stop if we have gone too far
-        for c_path in complete:
-            if path.absolute_distance > c_path.absolute_distance: break
+    queue.append(path.Path(goal, begin))
 
-        for q_path in queue:
-            if nb in q_path:
-                path_to_node = q_path.sub_path(nb)
-                if path.absolute_distance > path_to_node.absolute_distance: continue
-                elif path_to_node > path:
-                    queue.pop(queue.index(q-path))
 
-        queue.sort()
+    while True:
+        path = queue.pop()
+        node = path[-1]
+        for nb in node.neighbours:
+            # don't go backwards
+            if nb in path: continue
+            new_path = path.add_node(nb)
+
+            # stop if we have gone too far
+            for c_path in complete:
+                if path.absolute_distance > c_path.absolute_distance: break
+
+            for q_path in queue:
+                if nb in q_path:
+                    path_to_node = q_path.sub_path(nb)
+                    if path.absolute_distance > path_to_node.absolute_distance: continue
+                    elif path_to_node > path:
+                        queue.pop(queue.index(q-path))
+
+            queue.sort()
 
--- path.py
+++ path.py
@@ -1,12 +1,17 @@
 class Path(object):
     def __init__(self, goal, nodes=None):
         self.nodes = []
-        if nodes:
+        try: # If list is given:
+            iter(nodes)
             self.nodes = Nodes
+        except TypeError: # Else: start with the given node
+            if nodes:
+                self.start(nodes)
         self.total_distance = 0
         for i in range(len(self.nodes)):
             self.total_distance += self.nodes[i].measure_distance(self.nodes[i+1])
         self.absolute_distance = self.nodes[-1].measure_distance(goal)
+        self.goal = goal
 
     def add_node(node):
         new_nodes = self.nodes
@@ -23,6 +28,9 @@ class Path(object):
                 path.append(node)
             else:
                 return path
+
+    def start(self, node):
+        self.nodes.append(node)
 
     def __iter__(self):
         return self.nodes.__iter__()
