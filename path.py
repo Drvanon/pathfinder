64f43082301e57fbaacdee52d14575fa61a83520
Martijn Arts <arts.martijn@gmail.com>
2013-03-26 22:25:17 +0100

    Fixed some errors in path.py (better now)

--- path.py
+++ path.py
@@ -8,29 +8,28 @@ class Path(object):
             if nodes:
                 self.start(nodes)
         self.total_distance = 0
+        self.calc_distance()
+        self.goal = goal
+
+    def calc_distance(self):
         for i in range(len(self.nodes)):
             self.total_distance += self.nodes[i].measure_distance(self.nodes[i+1])
         self.absolute_distance = self.nodes[-1].measure_distance(goal)
-        self.goal = goal
 
-    def add_node(node):
+    def add_node(self, node):
         new_nodes = self.nodes
         new_nodes.append(node)
         path = Path(new_nodes)
         return path
 
-    def sub_path(node):
+    def sub_path(self, node):
         if not node in path:
-            return False
-        path = []
-        for i in self:
-            if i != node:
-                path.append(node)
-            else:
-                return path
+            raise LookupError('No such node ({}) in this path.'.format(node))
+        return Path(self.nodes[:self.nodes.index(node)])
 
     def start(self, node):
         self.nodes.append(node)
+        self.calc_distance()
 
     def __iter__(self):
         return self.nodes.__iter__()
